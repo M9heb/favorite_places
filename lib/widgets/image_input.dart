7e888af88ffc19e2c1de442e6903768827c9d906
M9heb <123528241+M9heb@users.noreply.github.com>
2025-06-15 21:52:18 +0430

    Takes the image by image picker package

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,10 +12,55 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    if (pickedImage == null) return;
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      label: Text("Add input"),
+      icon: Icon(Icons.camera),
+    );
+
+    if (_selectedImage != null) {
+      content = Stack(children: [
+        Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+        Positioned(
+          top: 12,
+          right: 12,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface.withAlpha(80),
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              onPressed: _takePicture,
+              icon: Icon(
+                Icons.refresh,
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ),
+        ),
+      ]);
+    }
+
     return Container(
+      clipBehavior: Clip.antiAlias,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(12)),
           border: Border.all(
@@ -23,11 +70,7 @@ class _ImageInputState extends State<ImageInput> {
       alignment: Alignment.center,
       height: 240,
       width: double.infinity,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        label: Text("Add input"),
-        icon: Icon(Icons.camera),
-      ),
+      child: content,
     );
   }
 }
